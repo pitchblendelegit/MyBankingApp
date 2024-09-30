87cce599bcd0c92daf3ff6ce345ed5c8fcad919f
pitchblendelegit <72884743+pitchblendelegit@users.noreply.github.com>
2024-09-30 16:02:27 +0700

    baru nih

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const home = () => {
+  const loggedIn = {firstName : 'Adrian'};
   return (
-    <div>home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type = "greeting"
+            title = "welcome"
+            user = {loggedIn?.firstName || 'Guest'}
+            subtext = "Access and Manage Your Account and Transactions Efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
